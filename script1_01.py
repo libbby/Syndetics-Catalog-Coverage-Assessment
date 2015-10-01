c9e6aa2d6bdc5d77c2ad52a85c3624628ae48d0b
Libby <libbyw@email.unc.edu>
2015-10-01 12:16:37 -0400

    Script now saves data as an actual .xml file. Character replacement for spaces added, to avoid trouble later.

--- script1_01.py
+++ script1_01.py
@@ -5,12 +5,16 @@ import xml.etree.ElementTree as ET
 import urllib
 
 MARC773 = raw_input("enter MARC773 field: ") # if using PowerShell, right click to paste
+MARC773 = MARC773.replace(" ", "_")
+#print MARC773
+
 endeca_url = "http://search.lib.unc.edu/search?Ntt=" + MARC773 + "&Ntk=Keyword&Nty=1&output-format=xml&facet-options=exclude-refinements&include-record-property=ICE+Chapter+Title&include-record-property=Syndetics+ISBN&include-record-property=OCLCNumber&include-record-property=UPC&include-record-property=Main+Author&include-record-property=Other+Authors&maximum-number-records=1000"
 
 
 webbrowser.open(endeca_url) # here's your XML file in browser, if you want it. Firefox allows spaces from MARC773.
 
-urllib.urlretrieve(endeca_url, str(MARC773)) # saves that XML into the same folder this script is in, the name is the MARC773 field
+urllib.urlretrieve(endeca_url, MARC773 + ".xml") # saves that XML into the same folder this script is in, the name is the MARC773 field
+
 
 
 
