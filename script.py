d913ae2e8bf980226afff507027aed90ab56c1c1
Libby <libbyw@email.unc.edu>
2015-10-05 14:21:36 -0400

    Changed to lxml library; XPath to generate counts of Other Authors and Syndetics ISBNs

--- script.py
+++ script.py
@@ -2,7 +2,8 @@
 # Libby Wilcher (https://github.com/libbby/Syndetics-Catalog-Coverage-Assessment)
 
 import webbrowser 
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+import lxml.etree as ET
 import urllib
 import csv
 
@@ -12,7 +13,7 @@ MARC773 = MARC773.replace(" ", "_")
 collection_short = raw_input("enter a 'shorthand' code for the collection: ")
 
 endeca_url = "http://search.lib.unc.edu/search?Ntt=" + MARC773 + "&Ntk=Keyword&Nty=1&output-format=xml&facet-options=exclude-refinements&include-record-property=ICE+Chapter+Title&include-record-property=Syndetics+ISBN&include-record-property=OCLCNumber&include-record-property=UPC&include-record-property=Main+Author&include-record-property=Other+Authors&maximum-number-records=1000"
-#webbrowser.open(endeca_url) # here's your XML file in browser, if you want it.
+webbrowser.open(endeca_url) # here's your XML file in browser, if you want it.
 urllib.urlretrieve(endeca_url, MARC773 + ".xml") 				# saves that XML into the same folder this script is in, the name is the MARC773 field
 
 tree = ET.parse(MARC773 + ".xml")
@@ -28,13 +29,12 @@ for node in child: 												# tells us (and script) what node is
 n = 0															# produces a count of ebooks in the given collection
 for node in child[1][0]:
 	n += 1
-
-
 	
 print ("There are " + str(n) + " ebooks in this collection.")
 
-c = csv.writer(open(MARC773 + ".csv", "wb"))
 # c = csv.writer(open("search.csv", "wb")) #test case for Kristina's UPC xml
+c = csv.writer(open(MARC773 + ".csv", "wb"))
+c.writerow(["Collection", "UNCb Identifier", "PK", "ICE ToC", "Main Author", "OCLC Number", "UPC", "Other Authors", "Syndetics ISBNs"])
 
 for node in child[0]:											
 	for i in range(n):											# this cycles through the ITEM tags that contain records data
@@ -50,20 +50,21 @@ for node in child[0]:
 		else:
 			bool_main_author = 0
 			
-		upc = child[1][0][i][1].find('UPC')
-		if upc != None:
-			bool_upc = 1
-		else:
-			bool_upc = 0
-			
 		oclc_number = child[1][0][i][1].find('OCLCNumber')
 		if oclc_number != None:
 			bool_oclc = 1
 		else:
 			bool_oclc = 0
 				
-		print child[1][0][i][0].text + ", " + str(bool_ice_toc) + ", " + str(bool_main_author) + ", " + str(bool_oclc) + ", " + str(bool_upc)
-		c.writerow([collection_short, child[1][0][i][0].text, collection_short + child[1][0][i][0].text, bool_ice_toc, bool_main_author, bool_oclc, bool_upc])		# comma delimited output... ADD TO THIS
+		upc = child[1][0][i][1].find('UPC')
+		if upc != None:
+			bool_upc = 1
+		else:
+			bool_upc = 0
+							
+		print child[1][0][i][0].text + ", " + str(bool_ice_toc) + ", " + str(bool_main_author) + ", " + str(bool_oclc) + ", " + str(bool_upc) + ", " + str(int(child[1][0][i][1].xpath("count(./Other-Authors/item)"))) + ", " + str(int(child[1][0][i][1].xpath("count(./Syndetics-ISBN/item)")))
+		#print child[1][0][i][1].xpath("count(./Other-Authors/item)")
+		c.writerow([collection_short, child[1][0][i][0].text, collection_short + child[1][0][i][0].text, bool_ice_toc, bool_main_author, bool_oclc, bool_upc, int(child[1][0][i][1].xpath("count(./Other-Authors/item)")), int(child[1][0][i][1].xpath("count(./Syndetics-ISBN/item)"))])		# comma delimited output... ADD TO THIS
 		
 
 
